865ff67d559f28a56d294cefabc775fbc30bd177
Yushi Xie <YushiXie@Yushis-MacBook-Pro.local>
2019-10-07 10:48:36 -0400

    WEB322 A2 Submission

---
+++ MyAirBnb.js
@@ -0,0 +1,40 @@
+/********************* IMPORT PACKAGES *********************/ 
+
+const express = require("express");
+const exphbs  = require('express-handlebars');
+
+/********************* CREATE OBJECT *********************/ 
+
+let app = express();
+
+/********************* REQUESTS *********************/ 
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static("public"));
+
+/********************* SET ROUTES *********************/ 
+
+app.get("/", (req, res)=>
+{
+    res.render('home');
+});
+
+app.get("/roomList", (req, res)=>
+{
+    res.render('roomList');
+});
+
+app.get("/userReg", (req, res)=>
+{
+    res.render('userReg');
+});
+
+/********************* LISTEN *********************/ 
+
+const PORT = 3000;
+app.listen(PORT, ()=>
+{
+    console.log(`Web Server is Connected to Port: ${PORT}`);
+});
