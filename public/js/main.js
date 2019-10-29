7bab4b832e034c33f5466d9fdf2ea4c9644b58fe
Yushi Xie <YushiXie@Yushis-MacBook-Pro.local>
2019-10-29 11:19:21 -0400

    A3 Update 3.0: Fixed Datebox boarder Issue

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,26 @@
+let alinktosignup = document.getElementById('signupbut');
+let alinktologin = document.getElementById('loginbut');
+
+alinktologin.onclick = function(event) {
+    if (window.matchMedia("(max-width: 500px)").matches) {
+        window.location.href = "/Login";
+    }
+    else
+    {
+        document.getElementById('id03').style.display='block';
+    }
+}
+
+alinktosignup.onclick = function(event) {
+    if (window.matchMedia("(max-width: 500px)").matches) {
+        window.location.href = "/SignUp";
+    }
+    else
+    {
+        document.getElementById('id01').style.display='block';
+    }
+}
+
 // Get the modal
 let modal1 = document.getElementById('id01');
 let modal2 = document.getElementById('id02');
@@ -18,34 +41,5 @@ window.onclick = function(event) {
         modal1.style.display = "none";
         modal2.style.display = "none";
         modal3.style.display = "none";
-    }
-}
-
-let alinktosignup = document.getElementById('signupbut');
-let alinktologin = document.getElementById('loginbut');
-
-alinktologin.onclick = function(event) {
-    if (window.matchMedia("(max-width: 500px)").matches) {
-        /* The viewport is less than, or equal to, 700 pixels wide */
-        //alert('Hi');
-        //return res.render('Login');
-        window.location.href = "/Login";
-    }
-    else
-    {
-        document.getElementById('id03').style.display='block';
-    }
-}
-
-alinktosignup.onclick = function(event) {
-    if (window.matchMedia("(max-width: 500px)").matches) {
-        /* The viewport is less than, or equal to, 700 pixels wide */
-        //alert('Hi');
-        //return res.render('Login');
-        window.location.href = "/SignUp";
-    }
-    else
-    {
-        document.getElementById('id01').style.display='block';
     }
 }
