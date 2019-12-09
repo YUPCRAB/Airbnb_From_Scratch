be7b868ffef31e8e8d8e3db44fd4a101c461ef12
Yushi Xie <YushiXie@Yushis-MacBook-Pro.local>
2019-12-09 09:51:25 -0500

    Assignment Final Submit Version

--- routes/General.js
+++ routes/General.js
@@ -48,19 +48,51 @@ router.post("/roomList/book/:id", hasAccessUser, (req, res)=>
 {
         Room.findById(req.params.id)
         .then((room) =>{
-            //console.log(`${room}`)
             User.findById(req.session.userInfo._id)
                 .then((user) =>{
-                    //console.log(`${user}`)
-                    user.BookedRooms.push(room)
-                    user.save()
-                    res.render('User/dash',
+                    if(user.BookedRooms.length==0)
                     {
-                        rooms:user.BookedRooms
-                    });
+                        user.BookedRooms.push(room)
+                        user.save()
+                        res.render('User/dash',
+                        {
+                            rooms:user.BookedRooms
+                        });
+                    }
+                    else
+                    {
+                        let flag = true;
+                        for (let i = 0; i < user.BookedRooms.length; i++)
+                        {
+                            if(req.params.id==user.BookedRooms[i]._id)
+                            {
+                                flag = false;
+                            }
+                        }
+                        if(flag)
+                        {
+                            user.BookedRooms.push(room)
+                            user.save()
+                            res.render('User/dash',
+                            {
+                                rooms:user.BookedRooms
+                            });
+                        }
+                        else
+                        {
+                            const errs = [];
+                            errs.push("You have already booked this room~");
+                            Room.find()
+                            .then((rooms)=>{
+                                res.render('General/roomList',
+                                {
+                                    message:errs,
+                                    rooms:rooms
+                                });
+                            })
+                        }
+                    }
                 })
-                
-            //req.session.userInfo.BookedRooms.push(room)
         })
         .catch(err=>console.log(`Error : ${err}`));    
 });
